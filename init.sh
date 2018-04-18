27655d876e5c46e2dfe3e05f28f73f6be0f5f3bd
CoralieNeubueser <coralie.neubuser@cern.ch>
2018-04-18 12:41:36 +0200

    keep link to calo analysis library

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh -u
 # set FCCSWBASEDIR to the directory containing this script
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -6,4 +5,4 @@ weekday=`date +%a`
 source /cvmfs/fcc.cern.ch/testing/sw/views/stable/x86_64-slc6-gcc62-opt/setup.sh
 # library path for py converter
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/work/c/cneubuse/public/FCC_calo_analysis_cpp/install/lib/
-
+add_to_path LD_LIBRARY_PATH /afs/cern.ch/user/j/jcervant/public/xerces/lib/
